dc48d4b377794c62dd0470b851296e3c8015c19f
SunilP <spottumuttu@osius.com>
2021-04-23 14:43:26 +0530

    initial version

---
+++ mySparkApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace mySparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Create a Spark session
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("word_count_sample")
+                .GetOrCreate();
+
+            // Create initial DataFrame
+            DataFrame dataFrame = spark.Read().Text("input.txt");
+
+            // Count words
+            DataFrame words = dataFrame
+                .Select(Functions.Split(Functions.Col("value"), " ").Alias("words"))
+                .Select(Functions.Explode(Functions.Col("words"))
+                .Alias("word"))
+                .GroupBy("word")
+                .Count()
+                .OrderBy(Functions.Col("count").Desc());
+
+            // Show results
+            words.Show();
+
+            // Stop Spark session
+            spark.Stop();
+        }
+    }
+}
