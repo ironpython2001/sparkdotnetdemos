e710fa3f01746c7a8b50fec1c72f2d3ec2c50b23
DESKTOP-PG56K6G\ipydev <ironpython2001@gmail.com>
2021-05-16 14:03:50 +0530

    modified

--- SparkDotnetCookBook/DataFrameApiDemo1/Program.cs
+++ SparkDotnetCookBook/DataFrameApiDemo1/Program.cs
@@ -16,8 +16,8 @@ namespace DataFrameApiDemo1
 
             var mySchema = new StructType(new List<StructField>
             {
-                new StructField("Name", new StringType()),
-                new StructField("Age", new IntegerType())
+                new StructField("Name", new StringType(),false),
+                new StructField("Age", new IntegerType(),false)
             });
             var myData = new List<GenericRow>
             {
@@ -26,16 +26,23 @@ namespace DataFrameApiDemo1
                 new GenericRow(new object[]{ "Jules", 30 }),
                 new GenericRow(new object[]{ "TD", 35 }),
                 new GenericRow(new object[]{ "Brooke", 25 }),
-
             };
 
             var data_df = spark.CreateDataFrame(myData, mySchema);
+
+            foreach(var f in data_df.Schema().Fields)
+            {
+                WriteLine(f.Name);
+                WriteLine(f.DataType.TypeName.ToString());
+            }
+
+            data_df.PrintSchema();
             data_df.Show();
             //Group the same names together, aggregate their ages, and compute an average
             var avg_df = data_df.GroupBy("Name").Agg(Avg("Age"));
             avg_df.Show();
 
-            
+
             spark.Stop();
         }
     }
