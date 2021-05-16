4554897d16a8ea47d29ac325943a624417a80742
DESKTOP-PG56K6G\ipydev <ironpython2001@gmail.com>
2021-05-16 13:42:36 +0530

    addd DataFrameApiDemo

---
+++ SparkDotnetCookBook/DataFrameApiDemo1/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+using Microsoft.Spark.Sql.Types;
+using System.Collections.Generic;
+
+namespace DataFrameApiDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession.Builder()
+                                   .AppName("AuthorsAges")
+                                   .GetOrCreate();
+
+            var mySchema = new StructType(new[]
+            {
+                new StructField("Name", new StringType()),
+                new StructField("Age", new IntegerType())
+            });
+            var myData = new List<GenericRow>
+            {
+                new GenericRow(new object[]{"Brooke", 20 }),
+                new GenericRow(new object[]{ "Denny", 31 }),
+                new GenericRow(new object[]{ "Jules", 30 }),
+                new GenericRow(new object[]{ "TD", 35 }),
+                new GenericRow(new object[]{ "Brooke", 25 }),
+
+            };
+
+            var data_df = spark.CreateDataFrame(myData, mySchema);
+            data_df.Show();
+
+            //Group the same names together, aggregate their ages, and compute an average
+            var avg_df = data_df.GroupBy("Name").Agg(Avg("Age"));
+            avg_df.Show();
+
+            
+            spark.Stop();
+        }
+    }
+}
---
+++ SparkDotnetCookBook/WordCount/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession.Builder()
+                                    .AppName("wordcount")
+                                    .GetOrCreate();
+
+            var mnm_df = spark.Read()
+                        .Format("csv")
+                        .Option("header", "true")
+                        .Option("inferSchema", "true")
+                        .Load("mnm_dataset.csv");
+            mnm_df.Show();
+            var count_mnm_df = mnm_df.Select("State", "Color", "Count")
+                                    .GroupBy("State", "Color")
+                                    .Agg(Count("Count")).ToDF("State", "Color", "Total")
+                                    .OrderBy("Total");
+
+            count_mnm_df.Show(60);
+            WriteLine($"total rows = {count_mnm_df.Count()}");
+
+            //just for CA state
+            var ca_count_mnm_df = mnm_df.Select("State", "Color", "Count")
+                                    .Where("State='CA'")
+                                    .GroupBy("State", "Color")
+                                    .Agg(Count("Count")).ToDF("State", "Color", "Total")
+                                    .OrderBy("Total");
+
+            ca_count_mnm_df.Show(20);
+
+            spark.Stop();
+        }
+    }
+}
